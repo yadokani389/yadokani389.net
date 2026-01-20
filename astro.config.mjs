810bf9c34605fa17123177541137510de4ea25a2
yadokani389 <yadokani389@gmail.com>
2026-01-20 13:08:08 +0900

    chore: set site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
+import { SITE_URL } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
+  site: import.meta.env.SITE_URL ?? SITE_URL,
   integrations: [svelte(), mdx()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,3 @@
 export const SITE_TITLE = "yadokani389";
 export const SITE_DESCRIPTION = "yadokani389のホームページ";
+export const SITE_URL = "https://yadokani389.net";

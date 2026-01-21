e4e26f37e866e1c75a443e1cdf1b89281c42ead9
yadokani389 <yadokani389@gmail.com>
2026-01-21 16:42:00 +0900

    chore(lint): add eslint and stylelint

---
+++ eslint.config.cjs
@@ -0,0 +1,44 @@
+const js = require("@eslint/js");
+const eslintPluginAstro = require("eslint-plugin-astro");
+const tsParser = require("@typescript-eslint/parser");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+
+module.exports = [
+  {
+    ignores: [".astro/**"],
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,cjs,mjs,ts,tsx}", "**/*.astro"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+  ...eslintPluginAstro.configs["flat/recommended"],
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: require("astro-eslint-parser"),
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: [".astro"],
+      },
+    },
+  },
+  {
+    files: ["eslint.config.cjs"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+];

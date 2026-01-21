21834c604da0cde3add5d08bac03015babbfbc49
yadokani389 <yadokani389@gmail.com>
2026-01-21 19:14:23 +0900

    enhance(ui): use tailwind

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,6 +1,27 @@
 module.exports = {
   extends: ["stylelint-config-recommended", "stylelint-config-html"],
   rules: {
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "tailwind",
+          "apply",
+          "layer",
+          "variants",
+          "responsive",
+          "screen",
+          "theme",
+          "plugin",
+          "source",
+          "utility",
+          "variant",
+          "custom-variant",
+          "reference",
+          "config",
+        ],
+      },
+    ],
     "no-descending-specificity": null,
   },
 };
